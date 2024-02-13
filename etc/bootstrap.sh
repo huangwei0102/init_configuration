627037de58f590de9fc0e789f80d9099ce57c565
huangwei0102 <569446624@qq.com>
2024-02-14 00:47:13 +0800

    Initial commit

---
+++ etc/bootstrap.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+# https://github.com/skywind3000/vim/raw/master/tools/script/bootstrap.sh
+
+set -e
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+# MSI: ssh-keygen -t rsa -b 4096 -C "
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJ7vNox06KpbNbdzlO8j3u4IfjsAvNKkeOWE9GSvhhsbPE5P1lhBALUkT4G8b5jZawR/7BsTiAzIUJghaBkPMy7RuGHKE3kMIeWhMcarjdEyT1hSTv68pmGEALHHwqFwgTKWEkvFCid5OHLUVnhenReRFglP8/X8ucIEN3JwyzS2ZrOGrVlXXrAbirV7GtkSOHQg1G5vObesizwz0L0JeyHnEFASv2wDnqCcZs3w9ymyuPmYV5qEFwx1/+50aVDrI+GSG/sErv/H9bJ2PdT96/UXcIVEe8CKz59PNIQEc4HUm5oxQ9nYkYOm7HgRnM6FJSCszU66RyCwz4z+SKQjTHzo0eeXBaMgxREJVqCGTrcENPfnBHpoMjdr7SRKmDd2HbvuKme/tsWCFJsXaZH+/qoCiaL9HAKO8bcOeqofZIct/FinCg4ZuU1BIvi1QCuHjNqv/PxbGkYDMxpGdNGlcAYTxvmFs9CXDq568NWSGMeYz7xSUH3p3Q0Tf0T+KztyTjRU+IDHoZEi4MPAx3cel8/8hPWODiCjFBUTC4mGbkihGWKv9uJODSg+2CnXkMPwzR7FpD90Kb/NlYj4UGMHjKTzUkUYItAxQEJixH6r+TXbw/i4QjnBbfGtG/tC3JWF26Zh23au/ALlp5UqMLWCJBGCM4kwiq+Gd4ZjIdyKHxNQ== 569446624@qq.com" >> authorized_keys
+chmod 600 authorized_keys
+
+mkdir -p ~/.local/init
+cd ~/.local/init
+
+git clone https://github.com/skywind3000/vim.git
+cd ~/.local/init/init_configuration
+sh update.sh
+
+echo 'source ~/.local/etc/init.sh' >> ~/.bashrc
+echo 'umask 022' >> ~/.bashrc
+
---
+++ etc/update.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+SCRIPT=$(readlink -f "$0" || echo "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+
+[ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2> /dev/null
+[ ! -d "$HOME/.local/etc" ] && mkdir -p "$HOME/.local/etc" 2> /dev/null
+[ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin" 2> /dev/null
+[ ! -d "$HOME/.local/lib" ] && mkdir -p "$HOME/.local/lib" 2> /dev/null
+[ ! -d "$HOME/.local/lib/python" ] && mkdir -p "$HOME/.local/lib/python" 2> /dev/null
+[ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config" 2> /dev/null
+
+
+# Define a function to copy matching files if they exist, does not perform copying if no matching files are found
+copy_if_exists() {
+    local source_path="$1"
+    local target_path="$2"
+    local pattern="$3"
+
+    # Enable nullglob to treat non-matching globs as empty strings
+    shopt -s nullglob
+    local files=($source_path/$pattern)
+
+    # Perform copying if matching files are found
+    if [ ${#files[@]} -gt 0 ]; then
+        cp "${files[@]}" "$target_path"
+    fi
+
+    # Disable nullglob option
+    shopt -u nullglob
+}
+
+# Function to extract all .tar.gz files from a specific directory to $HOME
+extract_tar_gz() {
+    local source_dir="$1"
+    local target_dir="$2"
+
+    shopt -s nullglob
+    local files=($source_dir/*.tar.gz)
+
+    for file in "${files[@]}"; do
+        tar -xzvf "$file" -C "$target_dir"
+    done
+
+    shopt -u nullglob
+}
+
+# Use the defined function to copy files
+copy_if_exists "$SCRIPTPATH" "$HOME/.local/etc" "*.sh"
+copy_if_exists "$SCRIPTPATH" "$HOME/.local/etc" "*.conf"
+copy_if_exists "$SCRIPTPATH" "$HOME/.local/etc" "*.zsh"
+copy_if_exists "$SCRIPTPATH" "$HOME/.local/etc" "*.lua"
+copy_if_exists "$SCRIPTPATH" "$HOME/.local/etc" "*.py"
+copy_if_exists "$SCRIPTPATH" "$HOME/.local/etc" "inputrc"
+
+# Special case: Copy all files in the directory if the directory exists
+copy_if_exists "$SCRIPTPATH/../tools/bin" "$HOME/.local/bin" "*"
+copy_if_exists "$SCRIPTPATH/../lib/python" "$HOME/.local/lib/python" "*"
+
+# Extract all .tar.gz files from $SCRIPTPATH/../tools/utils to $HOME
+extract_tar_gz "$SCRIPTPATH/../tools/utils" "$HOME"
+
+
+
